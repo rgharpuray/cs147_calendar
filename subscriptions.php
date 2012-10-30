b4a531e457b89d9382841043a84a57555ef1240b
Rishi <rgharpuray@gmail.com>
2012-10-30 15:12:28 -0700

    color coding based on subscriptions

--- subscriptions.php
+++ subscriptions.php
@@ -23,7 +23,8 @@
 		$(".nfl-teams").show();
 		$(".mlb-teams").hide();
 		$(".nba-teams").hide();
-		$(".calendar-item").click(function() {
+		$("#nfl").css("opacity", 1.0);
+		$(".team-item-subscribed").click(function() {
 			if($(this).css("background-color") == 'rgb(8, 50, 66)')
 			{
 				$(this).css("background-color", "green");
@@ -89,57 +90,72 @@
 	</div>
 
 	<div class="nfl-teams">
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://www.wallpaperpimper.com/wallpaper/Football/Minnesota_Vikings/Minnesota-Vikings-Logo-1-NB7GN30U93-1280x1024.jpg"/>
 			<p class="game-date">Minnesota Vikings </p>
-		
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://seahawks.sportspressnw.com/files/2010/01/seahawks_logo-bevel_bg_16001.jpg"/>
 			<p class="game-date">Seattle Seahawks</br>
 			</p>
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
+			<img class="home-team-logo" src = "http://images.pictureshunt.com/pics/c/cincinnati_bengals_logo-9085.gif"/>
+			<p class="game-date">Cincinatti Bengals</p>
+		</div>
+		
+		<div class="team-item-unsubscribed">
+			<img class="home-team-logo" src = "http://www.wallpaperpimper.com/wallpaper/Football/Minnesota_Vikings/Minnesota-Vikings-Logo-1-NB7GN30U93-1280x1024.jpg"/>
+			<p class="game-date">Minnesota Vikings </p>
+		</div>
+
+		<div class="team-item-unsubscribed">
+			<img class="home-team-logo" src = "http://seahawks.sportspressnw.com/files/2010/01/seahawks_logo-bevel_bg_16001.jpg"/>
+			<p class="game-date">Seattle Seahawks</br>
+			</p>
+		</div>
+
+		<div class="team-item-unsubscribed">
 			<img class="home-team-logo" src = "http://images.pictureshunt.com/pics/c/cincinnati_bengals_logo-9085.gif"/>
 			<p class="game-date">Cincinatti Bengals</p>
 		</div>
 	</div>
 	
 	<div class="nba-teams">
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://www.goodlogo.com/images/logos/chicago_bulls_logo_3120.gif"/>
 			<p class="game-date">Chicago Bulls </p>
 		
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://content.sportslogos.net/logos/6/219/full/852.gif"/>
 			<p class="game-date">Washington Wizards</br>
 			</p>
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Golden_State_Warriors.svg/200px-Golden_State_Warriors.svg.png"/>
 			<p class="game-date">Golden State Warriors</p>
 		</div>
 	</div>
 	  
 	<div class="mlb-teams">
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://upload.wikimedia.org/wikipedia/en/thumb/5/5e/SF_GiantsLogo_83_93.png/120px-SF_GiantsLogo_83_93.png"/>
 			<p class="game-date">San Fransisco Giants </p>
 		
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Baltimore_Orioles_Script.svg/220px-Baltimore_Orioles_Script.svg.png"/>
 			<p class="game-date">Baltimore Orioles</br>
 			</p>
 		</div>
 
-		<div class="calendar-item">
+		<div class="team-item-subscribed">
 			<img class="home-team-logo" src = "http://www.harvardclub.com/Images/Library/red-sox-logo.jpg"/>
 			<p class="game-date">Boston Red Sox</p>
 		</div>
